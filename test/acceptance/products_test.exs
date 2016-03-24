f9f2541e4e0604b7f672d4ae4f76ecd0b1771568
Seb Ashton <seb@sebashton.com>
2016-03-24 12:08:19 +0000

    Add test for product#index

--- test/acceptance/products_test.exs
+++ test/acceptance/products_test.exs
@@ -3,8 +3,21 @@ defmodule PhoenixCommerce.Acceptance.ProductsTest do
   use Hound.Helpers
   hound_session
 
-  test "/products has a list of products" do
+  test "/products - product details include name, description, and price" do
+    {:ok, product} =
+      %Product{
+        name: "Some product",
+        description: "Some product description",
+        price: Decimal.new("25.20")
+      } |> Repo.insert
+
     navigate_to "/products"
-    assert find_element(:css, "ul.products")
+    product_tr = find_element(:css, ".table tbody tr:first-child")
+    name = find_within_element(product_tr, :css, "td:first-child")
+    price = find_within_element(product_tr, :css, "td:last-child")
+    description = find_within_element(product_tr, :css, "td:nth-of-type(1)")
+    assert visible_text(name) == product.name
+    assert visible_text(price) == "$#{product.price}"
+    assert visible_text(description) == product.description
   end
 end
