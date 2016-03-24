c0c5f924cd12b00adb150110330e91c3b436ea0a
Seb Ashton <seb@sebashton.com>
2016-03-24 12:46:03 +0000

    Update tests

--- test/acceptance/products_test.exs
+++ test/acceptance/products_test.exs
@@ -3,21 +3,33 @@ defmodule PhoenixCommerce.Acceptance.ProductsTest do
   use Hound.Helpers
   hound_session
 
-  test "/products - product details include name, description, and price" do
+  setup do
+    Repo.delete_all(Product)
     {:ok, product} =
       %Product{
         name: "Some product",
         description: "Some product description",
         price: Decimal.new("25.20")
       } |> Repo.insert
+    {:ok, product: product}
+  end
 
+  test "/products - product details include name, description, and price", %{product: product} do
     navigate_to "/products"
-    product_tr = find_element(:css, ".table tbody tr:first-child")
-    name = find_within_element(product_tr, :css, "td:first-child")
-    price = find_within_element(product_tr, :css, "td:last-child")
-    description = find_within_element(product_tr, :css, "td:nth-of-type(1)")
+    product_li = find_element(:css, "ul.products li")
+    name = find_within_element(product_li, :css, "h3")
+    price = find_within_element(product_li, :css, "h4")
+    description = find_within_element(product_li, :css, "p")
     assert visible_text(name) == product.name
     assert visible_text(price) == "$#{product.price}"
     assert visible_text(description) == product.description
+  end
+
+  test "/products - click a product to view its details", %{product: product} do
+    navigate_to "/products"
+    product_li = find_element(:css, "ul.products li")
+    link = find_within_element(product_li, :css, "a")
+    click(link)
+    assert "/products/#{product.id}" == current_path
   end
 end
